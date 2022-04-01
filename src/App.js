cec040775185b62fa4a0c7aca0144ce80bbdc19c
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-04-01 15:49:36 +0600

    added example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import 
+import Example from "./Components/Example/Example";
+
 function App() {
-  const  example1 = ["pear", "yellow", "small"];
-  const example2 = ["toyota", "camry", "black", "expensive"];
-  const example3 = {name: "pear", color: "yellow", size: "small"};
-  const example4= {make: "toyota", model: "camry", price: "expensive", carColor:"black"};
-  // // const [name, color, size] = example1
-  // const [kind, mark, colorr , price] = example2
-  const {name, color, size } = example3
-  const {make, model, price, carColor} = example4
+  
   return (
     <div className="App">
-      This {name} is {color} and {size}.
-      This {make} {model} is {price} and {carColor}.
+      <Example/>
     </div>
   );
 }
+
 export default App;
