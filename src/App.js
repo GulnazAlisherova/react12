4a931564d75231d6bcfbd4c4df8dd7dec5335054
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-04-01 14:14:19 +0600

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 function App() {
-  const  example1 = ["Apple", "Red", "Big"];
+  const  example1 = ["pear", "yellow", "small"];
   return (
     <div className="App">
       This {example1[0]} is {example1[1]} and {example1[2]}.
