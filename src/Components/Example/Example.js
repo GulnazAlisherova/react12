6f579a089b367abb9b8843b150c970bdde2a3110
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-04-01 15:15:38 +0600

    added const

--- src/Components/Example/Example.js
+++ src/Components/Example/Example.js
@@ -1,16 +1,6 @@
 import { useState } from "react";
-function Example() {
+function Example()  {
+  const [fisrtName, setFirstName] = useState("");
 
-  const buttonCallback = (event) =>
-    alert("Hello world");
+  const 
 }
-
-//change
-
-return (
-  <div className="Example">
-    <input type="text"/>
-    <button onClick={buttonCallback}>Example</button>
-  </div>
-);
-export default Example;
