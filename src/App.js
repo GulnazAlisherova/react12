aeade9422ef5de7c4b94a8a2770709e4dbe0eff2
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-04-01 14:33:41 +0600

    added make, model e.t.c

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-
 import './App.css';
 function App() {
   const  example1 = ["pear", "yellow", "small"];
   const example2 = ["toyota", "camry", "black", "expensive"];
   const example3 = {name: "pear", color: "yellow", size: "small"};
-
-  // const [name, color, size] = example1
-  const [kind, mark, colorr , price] = example2
+  const example4= {make: "toyota", model: "camry", price: "expensive", carColor:"black"};
+  // // const [name, color, size] = example1
+  // const [kind, mark, colorr , price] = example2
   const {name, color, size } = example3
+  const {make, model, price, carColor} = example4
   return (
     <div className="App">
       This {name} is {color} and {size}.
-      This {kind} {mark} is {price} and {colorr}.
+      This {make} {model} is {price} and {carColor}.
     </div>
   );
 }
-
 export default App;
