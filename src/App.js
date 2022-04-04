bef9ae5bbda1471fe411d5487d56c1a3eba33bfd
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-04-04 13:52:10 +0600

    added example1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Example from "./Components/Example/Example";
-
+import Example1 from "./Components/Example/Example1";
 function App() {
   
   return (
     <div className="App">
       <Example/>
+      <Example1/>
     </div>
   );
 }
